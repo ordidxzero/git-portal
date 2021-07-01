2819f1bbe6c680f612976b4e7f92bf363d56f91a
ordidxzero <creator@hanyang.ac.kr>
2021-07-01 21:17:16 +0900

    Commander 적용

--- index.ts
+++ index.ts
@@ -1,23 +1,29 @@
-import fs from 'fs';
+import { Command } from 'commander';
+import readGitFile from './read-head';
 
-const f = fs
-  .readFileSync('.git/logs/HEAD', 'utf-8')
-  .split('\n')
-  .map(commit => commit.replace(/^([\w\-]+)/, '').trim())
-  .filter(Boolean)
-  .map(commit => commit.split('\t'))
-  .filter(commit => /^commit/.test(commit[1]))
-  .map(commit =>
-    commit.reduce((prev, cur, idx) => {
-      if (idx === 0) {
-        prev['id'] = cur.split(' ')[0];
-        return prev;
-      }
-      prev['message'] = cur.split(': ')[1];
-      return prev;
-    }, {} as any),
-  )
-  .map((commit, idx) => {
-    commit['index'] = idx;
-    return commit;
-  });
+const program = new Command('git-portal');
+
+program
+  .option('-o, --order <number>', 'Type order of commit. first = 0')
+  .option('-l, --last', 'order of last commit. If you use the o option, this option is ignored.');
+
+program.parse(process.argv);
+
+const options = program.opts();
+const logs = readGitFile();
+if (options.order) {
+  const order = +options.order;
+  if (Number.isNaN(order)) {
+    console.log(`Type only number!!!! range(0 ~ ${logs.length - 1})`);
+  } else {
+    if (order > logs.length - 1 || order < 0) {
+      console.log('This index is not valid');
+    } else {
+      console.log(logs[order]);
+    }
+  }
+} else {
+  if (options.last) {
+    console.log('Order Of Last Commit : ', logs.length - 1);
+  }
+}
--- read-head.ts
+++ read-head.ts
@@ -1,25 +1,26 @@
 import fs from 'fs';
 
-const f = fs
-  .readFileSync('.git/logs/HEAD', 'utf-8')
-  .split('\n')
-  .map(commit => commit.replace(/^([\w\-]+)/, '').trim())
-  .filter(Boolean)
-  .map(commit => commit.split('\t'))
-  .filter(commit => /^commit/.test(commit[1]))
-  .map(commit =>
-    commit.reduce((prev, cur, idx) => {
-      if (idx === 0) {
-        prev['id'] = cur.split(' ')[0];
+const readGitFile = () =>
+  fs
+    .readFileSync('.git/logs/HEAD', 'utf-8')
+    .split('\n')
+    .map(commit => commit.replace(/^([\w\-]+)/, '').trim())
+    .filter(Boolean)
+    .map(commit => commit.split('\t'))
+    .filter(commit => /^commit/.test(commit[1]))
+    .map(commit =>
+      commit.reduce((prev, cur, idx) => {
+        if (idx === 0) {
+          prev['id'] = cur.split(' ')[0];
+          return prev;
+        }
+        prev['message'] = cur.split(': ')[1];
         return prev;
-      }
-      prev['message'] = cur.split(': ')[1];
-      return prev;
-    }, {} as any),
-  )
-  .map((commit, idx) => {
-    commit['index'] = idx;
-    return commit;
-  });
+      }, {} as any),
+    )
+    .map((commit, idx) => {
+      commit['index'] = idx;
+      return commit;
+    });
 
-console.log(f);
+export default readGitFile;
