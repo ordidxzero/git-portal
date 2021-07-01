bdf9f699886e28e3797f329ec51af9c3640056b2
ordidxzero <creator@hanyang.ac.kr>
2021-07-01 16:57:55 +0900

    Fix bug

--- index.ts
+++ index.ts
@@ -1,5 +1,21 @@
 import fs from 'fs';
+import ChildProcess from 'child_process';
 
-const fl = fs.readdirSync('.');
+const formatter = (path: string) => (prev: any, cur: string, idx: number, arr: string[]) => {
+  const currentPath = `${path}/${cur}`;
+  const isFile = fs.lstatSync(currentPath).isFile();
+  if (isFile) {
+    const content = fs.readFileSync(currentPath, 'utf-8');
+    prev[cur] = content;
+    return prev;
+  }
+  const fl = fs.readdirSync(currentPath, 'utf-8').reduce(formatter(currentPath), {} as any);
+  prev[cur] = fl;
+  return prev;
+};
 
-console.log(fl);
+const f = fs.readdirSync('.git', 'utf-8').reduce(formatter('.git'), {} as any);
+
+fs.writeFileSync('git.json', JSON.stringify(f, null, 4));
+
+console.log(f);
