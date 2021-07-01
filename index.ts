d9dd3828c8b65ba265f684ae49cdc569b7d35e86
ordidxzero <creator@hanyang.ac.kr>
2021-07-01 21:39:08 +0900

    ChildProcess 적용

--- index.ts
+++ index.ts
@@ -1,29 +1,37 @@
 import { Command } from 'commander';
+import ChildProcess from 'child_process';
 import readGitFile from './read-head';
 
 const program = new Command('git-portal');
 
 program
   .option('-o, --order <number>', 'Type order of commit. first = 0')
-  .option('-l, --last', 'order of last commit. If you use the o option, this option is ignored.');
+  .option('-lc, --last', 'order of last commit. If you use the o option, this option is ignored.')
+  .option('-la , --latest', 'Back to the latest commit.');
 
 program.parse(process.argv);
 
 const options = program.opts();
 const logs = readGitFile();
-if (options.order) {
-  const order = +options.order;
-  if (Number.isNaN(order)) {
-    console.log(`Type only number!!!! range(0 ~ ${logs.length - 1})`);
-  } else {
-    if (order > logs.length - 1 || order < 0) {
-      console.log('This index is not valid');
-    } else {
-      console.log(logs[order]);
-    }
-  }
+if (options.latest) {
+  ChildProcess.exec('echo "This is latest"', function (error, stdout, stderr) {
+    console.dir(stdout);
+  });
 } else {
-  if (options.last) {
-    console.log('Order Of Last Commit : ', logs.length - 1);
+  if (options.order) {
+    const order = +options.order;
+    if (Number.isNaN(order)) {
+      console.log(`Type only number!!!! range(0 ~ ${logs.length - 1})`);
+    } else {
+      if (order > logs.length - 1 || order < 0) {
+        console.log('This index is not valid');
+      } else {
+        console.log(logs[order]);
+      }
+    }
+  } else {
+    if (options.last) {
+      console.log('Order Of Last Commit : ', logs.length - 1);
+    }
   }
 }
