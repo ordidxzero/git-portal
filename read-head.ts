b933bb4b05ef262d81b35cb88aad17bd5a05942b
ordidxzero <creator@hanyang.ac.kr>
2021-07-02 10:51:35 +0900

    Fix bug

--- read-head.ts
+++ read-head.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-export const filePAth = path.join(path.dirname(fs.realpathSync(__filename)), '../.git');
+export const filePAth = path.join(path.dirname(fs.realpathSync(__filename)), '../.git/logs/HEAD');
 
 const readGitFile = () =>
   fs
