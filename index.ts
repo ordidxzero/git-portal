d44fa16d00501e92af7d07b6c3f8f269abe7339b
ordidxzero <creator@hanyang.ac.kr>
2021-07-03 10:00:10 +0900

    Fix bug

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
+#! /usr/bin/env node
+
 import { Command } from 'commander';
 import ChildProcess from 'child_process';
 import readGitFile, { filePAth } from './read-head';
+import readPackageJson from './read-version';
 
 const program = new Command('git-portal');
 
@@ -14,35 +17,39 @@ program
 program.parse(process.argv);
 
 const options = program.opts();
-const logs = readGitFile();
-if (options.health) {
-  console.log(filePAth);
-}
-if (options.latest) {
-  ChildProcess.exec('git checkout -', (error, stdout, stderr) => {
-    if (stdout) {
-      console.log('Checkout to Latest Commit');
-    }
-  });
+if (options.version) {
+  console.log(readPackageJson());
 } else {
-  if (options.order) {
-    const order = +options.order;
-    if (Number.isNaN(order)) {
-      console.log(`Type only number!!!! range(0 ~ ${logs.length - 1})`);
-    } else {
-      if (order > logs.length - 1 || order < 0) {
-        console.log('This index is not valid');
-      } else {
-        ChildProcess.exec(`git checkout ${logs[order].id}`, (error, stdout, stderr) => {
-          if (stdout) {
-            console.log(`Checkout to "${logs[order].message}" (Order: ${order})`);
-          }
-        });
+  const logs = readGitFile();
+  if (options.health) {
+    console.log(filePAth);
+  }
+  if (options.latest) {
+    ChildProcess.exec('git checkout -', (error, stdout, stderr) => {
+      if (stdout) {
+        console.log('Checkout to Latest Commit');
       }
-    }
+    });
   } else {
-    if (options.last) {
-      console.log('Order Of Last Commit : ', logs.length - 1);
+    if (options.order) {
+      const order = +options.order;
+      if (Number.isNaN(order)) {
+        console.log(`Type only number!!!! range(0 ~ ${logs.length - 1})`);
+      } else {
+        if (order > logs.length - 1 || order < 0) {
+          console.log('This index is not valid');
+        } else {
+          ChildProcess.exec(`git checkout ${logs[order].id}`, (error, stdout, stderr) => {
+            if (stdout) {
+              console.log(`Checkout to "${logs[order].message}" (Order: ${order})`);
+            }
+          });
+        }
+      }
+    } else {
+      if (options.last) {
+        console.log('Order Of Last Commit : ', logs.length - 1);
+      }
     }
   }
 }
