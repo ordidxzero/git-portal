f697d368e31c5bb24eba472d96f1e425bcb36e2e
ordidxzero <creator@hanyang.ac.kr>
2021-07-03 10:02:00 +0900

    Fix path bug

--- read-version.ts
+++ read-version.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const filePath = path.join(process.cwd(), './package.json');
+const filePath = path.join('./package.json');
 
 const readPackageJson = () => {
   const json = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
